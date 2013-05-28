b62b0ae6fb656e0ee024cd0340735aec9c8c6441
Martin <martin.xu@linux.intel.com>
2013-05-28 10:11:50 +0800

    add test bluez_adapter_get_property_powered()
    Change-Id: I4c89385434d25584a723d98b95530e6320ad76e8
    Signed-off-by: Wu Jiangbo <jiangbox.wu@intel.com>

--- main.c
+++ main.c
@@ -6,9 +6,34 @@ GMainLoop *loop;
 
 void *adapter;
 
+gboolean idle_work(gpointer user_data)
+{
+
+	static uint times;
+
+	gboolean powered;
+
+	times++;
+
+	if (!bluez_adapter_get_property_powered(adapter, &powered)) {
+		DBG("adapter hci0 state %d", powered);
+
+		bluez_adapter_set_powered(adapter, !powered);
+	} else
+		ERROR("get adapter hci0 state error");
+
+	if (times > 10) {
+		bluez_lib_deinit();
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
 int main(int argc, char **argv)
 {
 	int ret;
+	gboolean powered;
 
 	g_type_init();
 
@@ -17,8 +42,13 @@ int main(int argc, char **argv)
 	bluez_lib_init();
 
 	adapter = bluez_adapter_get_adapter("hci0");
-	if (adapter != NULL)
-		bluez_adapter_set_powered(adapter, 0);
+	if (adapter != NULL) {
+		bluez_adapter_set_powered(adapter, 1);
+
+		idle_work(NULL);
+
+		g_timeout_add(2000, idle_work, NULL);
+	}
 
 	g_main_loop_run(loop);
 
