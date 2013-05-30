e5e10f4d866f2d818e0e3e91e6a0b093aa5d0801
Martin <martin.xu@linux.intel.com>
2013-05-30 17:17:22 +0800

    adapter: add function bluez_adapter_get_device()
    Change-Id: Ibc671b5c2b6f653638ad8a00e88dfa5237bcd725
    Signed-off-by: Wu Jiangbo <jiangbox.wu@intel.com>

--- bluez.h
+++ bluez.h
@@ -7,4 +7,7 @@ typedef struct _bluez_adapter bluez_adapter_t;
 typedef void (*bluez_adapter_powered_cb_t) (bluez_adapter_t *adapter, gboolean powered, gpointer user_data);
 
 typedef void (*bluez_adapter_alias_cb_t) (bluez_adapter_t *adapter, const gchar **alias, gpointer user_data);
+
+struct _bluez_device;
+typedef struct _bluez_device bluez_device_t;
 #endif
