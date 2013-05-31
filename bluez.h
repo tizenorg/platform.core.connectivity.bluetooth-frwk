b34a84b36bdc8c0f56d5fde9c0f3f419572c1d3b
Martin <martin.xu@linux.intel.com>
2013-05-31 11:24:58 +0800

    clean up code
    Change-Id: Ife70fc9ef15cdbd91df3c176f4e156818fd2b86f
    Signed-off-by: Wu Jiangbo <jiangbox.wu@intel.com>

--- bluez.h
+++ bluez.h
@@ -1,17 +1,62 @@
 #ifndef __BLUEZ_H__
 #define __BLUEZ_H__
+
 struct _bluez_adapter;
-
 typedef struct _bluez_adapter bluez_adapter_t;
-
-typedef void (*bluez_adapter_powered_cb_t) (bluez_adapter_t *adapter,
-						gboolean powered,
-						gpointer user_data);
-
-typedef void (*bluez_adapter_alias_cb_t) (bluez_adapter_t *adapter,
-						const gchar **alias,
-						gpointer user_data);
 
 struct _bluez_device;
 typedef struct _bluez_device bluez_device_t;
+
+
+void bluez_lib_deinit(void);
+int bluez_lib_init(void);
+
+/* adapter functions */
+struct _bluez_adapter *bluez_adapter_get_adapter(
+				const char *name);
+
+void bluez_adapter_start_discovery(
+				struct _bluez_adapter *adapter);
+void bluez_adapter_stop_discovery(
+				struct _bluez_adapter *adapter);
+
+struct _bluez_device *bluez_adapter_get_device(
+				struct _bluez_adapter *adapter,
+				const char *addr);
+void bluez_adapter_remove_device(
+				struct _bluez_adapter *adapter,
+				struct _bluez_device *device);
+
+int bluez_adapter_get_property_powered(
+				struct _bluez_adapter *adapter,
+				gboolean *powered);
+void bluez_adapter_set_powered(struct _bluez_adapter *adapter,
+				gboolean powered);
+typedef void (*bluez_adapter_powered_cb_t)(
+				bluez_adapter_t *adapter,
+				gboolean powered,
+				gpointer user_data);
+void bluez_adapter_set_powered_changed_cb(
+				struct _bluez_adapter *adapter,
+				bluez_adapter_powered_cb_t cb,
+				gpointer user_data);
+
+void bluez_adapter_set_alias(
+				struct _bluez_adapter *adapter,
+				const gchar *alias);
+typedef void (*bluez_adapter_alias_cb_t)(
+				bluez_adapter_t *adapter,
+				const gchar **alias,
+				gpointer user_data);
+void bluez_adapter_set_alias_changed_cb(
+				struct _bluez_adapter *adapter,
+				bluez_adapter_alias_cb_t cb,
+				gpointer user_data);
+
+/* device functions */
+void bluez_device_pair(struct _bluez_device *device);
+
+int bluez_device_property_get_adapter(
+				struct _bluez_device *device,
+				const char **adapter_path);
 #endif
