4e34239ecf807d66a45bd5db87469dc76e92f1bd
Wu Zheng <wu.zheng@intel.com>
2014-02-19 02:39:27 -0500

    Add the map agent skeleton
    Change-Id: Iecadd3731f3a60f6e69112291d0751d88b927682
    Signed-off-by: Wu Zheng <wu.zheng@intel.com>

---
+++ src/map_agent.c
@@ -0,0 +1,15 @@
+#include "common.h"
+#include "gdbus.h"
+#include "comms_error.h"
+#include "bluez.h"
+#include "vertical.h"
+
+void bt_map_agent_init(void)
+{
+	DBG("");
+}
+
+void bt_map_agent_deinit(void)
+{
+	DBG("");
+}
---
+++ src/map_agent.h
@@ -0,0 +1,11 @@
+#ifndef __MAP_AGENT__
+#define __MAP_AGENT__
+
+#include <gio/gio.h>
+#include "bluez.h"
+
+void bt_map_agent_init(void);
+
+void bt_map_agent_deinit(void);
+
+#endif
