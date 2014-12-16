16cafdea193ac9edf72f53d2516271e1ec7f1a7a
wu zheng <wu.zheng@intel.com>
2014-12-16 16:07:03 +0800

    Add the Tizen-2.3 CAPI bt_adapter_recover
    bt_adapter_recover doesn't exist in Tizen before Tizen-2.3.
    Add it to match Tizen-2.3.
    
    Change-Id: If9951cfaa2897c1a10823e77e8c2c30699669bc2
    Signed-off-by: Wu Zheng <wu.zheng@intel.com>

--- test/bluez-capi-test.c
+++ test/bluez-capi-test.c
@@ -119,6 +119,19 @@ static int disable(const char *p1, const char *p2)
 	return 0;
 }
 
+static int recover(const char *p1, const char *p2)
+{
+	int err;
+
+	err = bt_adapter_recover();
+	if (err != BT_SUCCESS) {
+		ERROR("bt_adapter_enable error: %d", err);
+		return 0;
+	}
+
+	return 0;
+}
+
 static int get_adapter_state(const char *p1, const char *p2)
 {
 	int err;
@@ -2265,6 +2278,9 @@ struct {
 		"Usage: deinit\n\tDe-initialize bluez-lib"},
 
 	{"enable", enable,
+		"Usage: enable\n\tEnable the local adapter"},
+
+	{"recover", recover,
 		"Usage: enable\n\tEnable the local adapter"},
 
 	{"disable", disable,
