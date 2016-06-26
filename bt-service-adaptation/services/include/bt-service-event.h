d61646af8c28765afbc75395f9914f7e4ce86f44
Anupam Roy <anupam.r@samsung.com>
2016-06-26 03:57:32 -0400

    Separate bluetooth service dir
    This initial patch does following:-
    
    1/Create bluetooth service adaptation
     directory(bt-service-adaptation), which will be used
     for stack adaptation development.
    
    2/Create services folder in bt-service-adaptation. All
    core services (hid, audio, opp, hfp, gatt etc) will be
    created inside the services dir in subsequent patches.
    
    3/Create basic files for bt-service initially inside
    bt-service-adaptation/services dir.
    
    3/Declare environment variable to enable build of
     bt-service-adaptation directory. [If bt_hal is enabled,
     bt-service-adaptation will be built, otherwise bt-service
     will be built]
    
    [Note: Functionalities will be enabled in subsequent patches]
    
    Change-Id: I4ce5bd9446896fed2c28294b01ac00d276a64450
    Signed-off-by: Anupam Roy <anupam.r@samsung.com>

---
+++ bt-service-adaptation/services/include/bt-service-event.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+
+#ifndef _BT_SERVICE_EVENT_H_
+#define _BT_SERVICE_EVENT_H_
+
+#include <sys/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int _bt_send_event(int event_type, int event, GVariant *param);
+
+int _bt_send_event_to_dest(const char* dest, int event_type, int event, GVariant *param);
+
+int _bt_init_service_event_sender(void);
+void _bt_deinit_service_event_sender(void);
+
+int _bt_init_service_event_receiver(void);
+void _bt_deinit_service_event_receiver(void);
+
+int _bt_opp_client_event_init(void);
+void _bt_opp_client_event_deinit(void);
+
+int _bt_send_hf_local_term_event(char *address);
+int _bt_init_hf_local_term_event_sender(void);
+void _bt_deinit_hf_local_term_event_sender(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /*_BT_SERVICE_EVENT_H_*/
+
